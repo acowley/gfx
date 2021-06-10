96d472da00493ab2746e7025c6e92a5f216773fa
Anthony Cowley <acowley@gmail.com>
2021-06-10 12:41:17 -0400

    Fix a compiler warning

--- src/backend/gl/src/info.rs
+++ src/backend/gl/src/info.rs
@@ -658,7 +658,7 @@ pub(crate) fn query_all(
 
     let mut features = Features::NDC_Y_UP | Features::MUTABLE_COMPARISON_SAMPLER;
     // TODO: Fill out downlevel features correctly.
-    let mut downlevel = hal::DownlevelProperties::all_enabled();
+    let _downlevel = hal::DownlevelProperties::all_enabled();
     // TODO: Merge downlevel/legacy features?
     let mut legacy = LegacyFeatures::empty();
 
